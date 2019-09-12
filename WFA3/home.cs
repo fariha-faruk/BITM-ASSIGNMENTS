aab8903b09ec8f34cbf95143a49a0fc1c2c5d3ed
Web App Develop - .Net <hridoy1109024@gmail.com>
2019-09-12 15:46:20 +0600

    windowsApp

---
+++ WFA3/home.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA3
+{
+    public partial class home : Form
+    {
+        private object showLabel;
+
+        public home()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello " + nameTextBox.Text);
+            showLabel1.Text = nameTextBox.Text;
+
+        }
+
+      
+    }
+}
